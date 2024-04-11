f1e9fc8138a8fcdf0f1691d121328447287e7d77
Konstantin <122147612+kharlamov39@users.noreply.github.com>
2024-04-11 14:53:49 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,10 +1,10 @@
 docker compose up --build -d
 echo 'First'
-docker compose exec -d php-cli bash -c "chmod 777 -R /var/www && composer create-project laravel/laravel name_project && php artisan key:generate"
+docker compose exec -d php-cli bash -c "chmod 777 -R /var/www && composer create-project laravel/laravel name_project"
 echo 'Second'
 sudo chmod 777 -R . 
 echo 'Three'
 mv name_project/* ./
 mv name_project/.* ./
 rm -rf name_project/
-xdg-open http://localhost:8080
+curl -sS http://localhost:8080 | xargs xdg-open
