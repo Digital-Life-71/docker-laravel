f9a855416949c1d8c51005d2313c9eec600e5d88
Konstantin <122147612+kharlamov39@users.noreply.github.com>
2024-04-11 13:48:48 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,7 @@
 docker compose up --build -d
+echo 'Fisrst'
 docker compose exec php-cli bash
+echo 'Second'
 chmod 777 -R /var/www
+echo 'Third'
 composer create-project laravel/laravel name_project
