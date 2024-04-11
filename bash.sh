b6fb58df1c549f9d91329648e7419c6519103ad7
Konstantin <122147612+kharlamov39@users.noreply.github.com>
2024-04-11 14:05:59 +0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,6 +2,6 @@ docker compose up --build -d
 echo 'First'
 docker compose exec -d php-cli bash -c "chmod 777 -R /var/www && composer create-project laravel/laravel name_project"
 echo 'Second'
-docker compose down
-sudo chmod 777 -R name_project/
+sudo chmod 777 -R . 
 echo 'Three'
+
